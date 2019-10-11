07f294fb689e1a765ecb8a8683f9f6c2adfabe8b
AAA <li2185@purdue.edu>
2019-10-11 18:57:47 -0400

    start

--- RBTree/RBTree.c
+++ RBTree/RBTree.c
@@ -2,17 +2,51 @@
 #define TEST_H
 #include <stdio.h>
 #include "test.h"
-void insert(struct treehead *tree, int num) {
+struct treenode * getparent(struct treenode *node) {
+  return node->parent;
+}
 
+int insert(struct treehead *tree, int num) {
+  struct treenode *node, *parent, *sibling, *grandparent, *newnode;
+  newnode = malloc(sizeof(struct treenode));
+  newnode->parent = NULL;
+  newnode->left = NULL;
+  newnode->right = NULL;
+  newnode->color = red;
+  
+
+  // empty tree
+  if (tree->count == 0) {
+    tree->head = newnode;
+    newnode->color = black;
+    tree->count = 1;
+    return 0;
+  }
+  
+  node = tree->head;
+  while (node != NULL) {
+    if (node->key > num) {
+      if (node->left == NULL) {
+        node->left = newnode;
+        break;
+      }
+      node = node->left;
+    }
+    else {
+      if (node->right == NULL) {
+        node->right == newnode;
+        newnode->parent = node;
+        tree->count++;
+        balancer(newnode);
+      }
+      node = node->right;
+    }
+  }
+  
 }
 
 int remove(struct treehead *tree, int num) {
-
-}
-
-v
-int main() {
-  
   return 0;
 }
+
 #endif
--- RBTree/RBTree.h
+++ RBTree/RBTree.h
@@ -1,12 +1,12 @@
 enum rbcolor {red, black};
-struct node {
+struct treenode {
   struct node *parent;
   struct node *left;
   struct node *right;
   enum rbcolor color;
   int key;
-}
+};
 struct treehead {
   struct node *head;
   int count;
-}
+};d
